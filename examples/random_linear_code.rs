cf2191a4ccc4d1774e660507282bb158e12e5bfd
maxtremblay <matrem@protonmail.com>
2020-12-05 17:48:20 -0500

    update sparse bin mat crate

--- examples/random_linear_code.rs
+++ examples/random_linear_code.rs
@@ -3,10 +3,13 @@ use rand::thread_rng;
 
 fn main() {
     let code = LinearCode::random_regular_code()
-        .block_size(20)
-        .number_of_checks(12)
+        .block_size(4)
+        .number_of_checks(3)
         .bit_degree(3)
-        .check_degree(5)
+        .check_degree(4)
         .sample_with(&mut thread_rng());
     println!("{:?}", code);
+    println!("Block size: {}", code.block_size());
+    println!("Dimension: {}", code.dimension());
+    println!("Minimal distance: {:?}", code.minimal_distance());
 }
