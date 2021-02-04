4c9397a2bd4b56ca3da9ae627adb6708fd4cd2a3
maxtremblay <matrem@protonmail.com>
2021-02-04 13:27:39 -0500

    Push to version 0.3.0

--- src/decoders/flip.rs
+++ src/decoders/flip.rs
@@ -1,40 +1,48 @@
 use crate::{LinearCode, SparseBinVec};
 use sparse_bin_mat::SparseBinVecBase;
+use std::borrow::Borrow;
 
 #[derive(Debug)]
-pub struct FlipDecoder<'code> {
-    code: &'code LinearCode,
+pub struct FlipDecoder<Code> {
+    code: Code,
 }
 
-impl<'code> From<&'code LinearCode> for FlipDecoder<'code> {
-    fn from(code: &'code LinearCode) -> Self {
+impl<Code> FlipDecoder<Code> {
+    pub fn new(code: Code) -> Self {
         Self { code }
     }
 }
 
-impl<'code> FlipDecoder<'code> {
+impl<Code> FlipDecoder<Code>
+where
+    Code: Borrow<LinearCode>,
+{
     pub fn decode<T>(&self, message: &SparseBinVecBase<T>) -> SparseBinVec
     where
         T: std::ops::Deref<Target = [usize]>,
     {
-        let mut syndrome = self.code.syndrome_of(message);
+        let mut syndrome = self.code().syndrome_of(message);
         let mut output = SparseBinVec::new(message.len(), message.as_slice().to_vec());
         while let Some(bit) = self.find_flippable(&syndrome) {
-            let update = SparseBinVec::new(self.code.block_size(), vec![bit]);
-            syndrome = &syndrome + &self.code.syndrome_of(&update);
+            let update = SparseBinVec::new(self.code().block_size(), vec![bit]);
+            syndrome = &syndrome + &self.code().syndrome_of(&update);
             output = &output + &update;
         }
         output
     }
 
     fn find_flippable(&self, syndrome: &SparseBinVec) -> Option<usize> {
-        self.code.bit_adjacencies().rows().position(|checks| {
+        self.code().bit_adjacencies().rows().position(|checks| {
             let number_unsatisfied = checks
                 .non_trivial_positions()
                 .filter(|check| syndrome.is_one_at(*check).unwrap_or(false))
                 .count();
             number_unsatisfied > checks.weight() / 2
         })
+    }
+
+    fn code(&self) -> &LinearCode {
+        self.code.borrow()
     }
 }
 
@@ -45,7 +53,7 @@ mod test {
     #[test]
     fn no_error_for_hamming_code() {
         let code = LinearCode::hamming_code();
-        let decoder = FlipDecoder::from(&code);
+        let decoder = FlipDecoder::new(code);
         let error = SparseBinVec::new(7, Vec::new());
         println!("OK");
         assert_eq!(decoder.decode(&error), SparseBinVec::zeros(7));
@@ -54,7 +62,7 @@ mod test {
     #[test]
     fn flipping_first_bit_for_hamming_code() {
         let code = LinearCode::hamming_code();
-        let decoder = FlipDecoder::from(&code);
+        let decoder = FlipDecoder::new(code);
         let codeword = SparseBinVec::new(7, vec![0, 1, 2]);
         let error = SparseBinVec::new(7, vec![0]);
         let corrupted = &codeword + &error;
@@ -64,7 +72,7 @@ mod test {
     #[test]
     fn flipping_third_bit_for_hamming_code() {
         let code = LinearCode::hamming_code();
-        let decoder = FlipDecoder::from(&code);
+        let decoder = FlipDecoder::new(&code);
         let codeword = SparseBinVec::new(7, vec![3, 4, 5, 6]);
         let error = SparseBinVec::new(7, vec![2]);
         let corrupted = &codeword + &error;
@@ -75,7 +83,7 @@ mod test {
     #[test]
     fn flipping_first_and_third_bit_for_hamming_code() {
         let code = LinearCode::hamming_code();
-        let decoder = FlipDecoder::from(&code);
+        let decoder = FlipDecoder::new(&code);
         let codeword = SparseBinVec::new(7, vec![0, 2, 4, 6]);
         let error = SparseBinVec::new(7, vec![0, 2]);
         let corrupted = &codeword + &error;
